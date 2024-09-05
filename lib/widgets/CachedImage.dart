082d75509877159de4cce7d521b373ea3d7617fc
mohamed abdelrahman <mohamed.abdelrahman6299@gmail.com>
2024-09-05 07:15:06 +0300

    cached image

--- lib/widgets/CachedImage.dart
+++ lib/widgets/CachedImage.dart
@@ -3,13 +3,10 @@ import 'package:tf_custom_widgets/utils/WidgetUtils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-
-
-class CachedImage extends StatelessWidget{
-
+class CachedImage extends StatelessWidget {
   final String url;
   final BoxFit? fit;
-  final double? height,width, borderWidth;
+  final double? height, width, borderWidth;
   final BorderRadius? borderRadius;
   final ColorFilter? colorFilter;
   final Alignment? alignment;
@@ -19,26 +16,24 @@ class CachedImage extends StatelessWidget{
   final Color? bgColor;
   final BoxShape? boxShape;
   final bool haveRadius;
-  CachedImage({
-    required this.url,
-    this.fit,
-    this.width,
-    this.height,
-    this.placeHolder,
-    this.borderRadius,
-    this.colorFilter,
-    this.alignment,
-    this.child,
-    this.boxShape,
-    this.borderColor,
-    this.borderWidth,
-    this.bgColor,
-    this.haveRadius=true
-  });
+  CachedImage(
+      {required this.url,
+      this.fit,
+      this.width,
+      this.height,
+      this.placeHolder,
+      this.borderRadius,
+      this.colorFilter,
+      this.alignment,
+      this.child,
+      this.boxShape,
+      this.borderColor,
+      this.borderWidth,
+      this.bgColor,
+      this.haveRadius = true});
 
   @override
   Widget build(BuildContext context) {
-
     return CachedNetworkImage(
       imageUrl: "$url",
       width: width,
@@ -49,24 +44,29 @@ class CachedImage extends StatelessWidget{
         decoration: BoxDecoration(
           image: DecorationImage(
               image: imageProvider,
-              fit: fit??BoxFit.fill,
-              colorFilter: colorFilter
-          ),
-          borderRadius: haveRadius? borderRadius??BorderRadius.circular(0):null,
-          shape: boxShape??BoxShape.rectangle,
-          border: Border.all(color: borderColor??Colors.transparent,width: borderWidth??1),
+              fit: fit ?? BoxFit.fill,
+              colorFilter: colorFilter),
+          borderRadius:
+              haveRadius ? borderRadius ?? BorderRadius.circular(0) : null,
+          shape: boxShape ?? BoxShape.rectangle,
+          border: Border.all(
+              color: borderColor ?? Colors.transparent,
+              width: borderWidth ?? 1),
         ),
-        alignment: alignment??Alignment.center,
+        alignment: alignment ?? Alignment.center,
         child: child,
       ),
       placeholder: (context, url) => Container(
-        width: width,height: height,
+        width: width,
+        height: height,
         alignment: Alignment.center,
         decoration: BoxDecoration(
-            borderRadius: haveRadius? borderRadius??BorderRadius.circular(0):null,
-            border: Border.all(color: borderColor??Colors.transparent,width: 1),
-            shape: boxShape??BoxShape.rectangle,
-            color: bgColor??WidgetUtils.primaryColor.withOpacity(.5),
+          borderRadius:
+              haveRadius ? borderRadius ?? BorderRadius.circular(0) : null,
+          border:
+              Border.all(color: borderColor ?? Colors.transparent, width: 1),
+          shape: boxShape ?? BoxShape.rectangle,
+          color: bgColor ?? WidgetUtils.primaryColor.withOpacity(.5),
         ),
         child: SpinKitFadingCircle(
           color: WidgetUtils.primaryColor,
@@ -74,22 +74,24 @@ class CachedImage extends StatelessWidget{
         ),
       ),
       errorWidget: (context, url, error) => Container(
-        width: width,height: height,
+        width: width,
+        height: height,
         alignment: Alignment.center,
         decoration: BoxDecoration(
-            color: bgColor??WidgetUtils.primaryColor.withOpacity(.5),
-            borderRadius: haveRadius? borderRadius??BorderRadius.circular(0):null,
-            border: Border.all(color: borderColor??Colors.transparent,width: 1),
-            shape: boxShape??BoxShape.rectangle,
+          color: bgColor ?? WidgetUtils.primaryColor.withOpacity(.5),
+          borderRadius:
+              haveRadius ? borderRadius ?? BorderRadius.circular(0) : null,
+          border:
+              Border.all(color: borderColor ?? Colors.transparent, width: 1),
+          shape: boxShape ?? BoxShape.rectangle,
         ),
         child: Stack(
           children: [
-            placeHolder??child??Container(),
-            child??Container(),
+            placeHolder ?? child ?? Container(),
+            child ?? Container(),
           ],
         ),
       ),
     );
   }
-
 }
